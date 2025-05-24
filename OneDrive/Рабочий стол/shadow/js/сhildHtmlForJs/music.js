519d8f407777aa8e816fd395700950f249c6463f
Windbreaker <fbk13102008@mail.ru>
2025-05-24 22:39:21 +0300

    I created a music page. And worked on the logic for it using js

---
+++ OneDrive/Рабочий стол/shadow/js/сhildHtmlForJs/music.js
@@ -0,0 +1,53 @@
+const player = document.getElementById('myPlayer');
+const buttons = document.querySelectorAll('.playSound');
+
+const tracksArr = Array.from(buttons).map(button => {
+    const match = button.getAttribute('onclick')?.match(/['"]([^'"]+)['"]/);
+    return {
+        url: match ? match[1] : '',
+        element: button
+    };
+});
+
+let count = 0;
+
+player.addEventListener('ended', nextTrack);
+
+function playSound(url) {
+    const index = tracksArr.findIndex(track => track.url === url);
+    if (index !== -1) {
+        count = index;
+        player.src = url;
+        player.play(); 
+    }
+}
+
+function nextTrack() {
+    count = (count + 1) % tracksArr.length;
+    player.src = tracksArr[count].url;
+    player.play(); 
+}
+
+buttons.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const match = btn.getAttribute('onclick')?.match(/['"]([^'"]+)['"]/);
+        const url = match ? match[1] : '';
+        if (url) {playSound(url)} ;
+        btn.classList.add('pause');
+        btn.classList.remove('play');
+
+        btn.addEventListener('click' , () => { 
+            if(btn.classList.contains('pause')){ 
+                btn.classList.remove('pause');
+                btn.classList.add('play');
+                player.pause();
+            }else if( btn.classList.contains('play')){ 
+                btn.classList.remove('play');
+                btn.classList.add('pause');
+                player.play();
+            }
+        })
+
+    })
+})
